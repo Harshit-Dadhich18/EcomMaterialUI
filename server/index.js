3c71c25d344fa8096fe5223fd78a923bfea73953
HARSHIT <hdadheech46@gmail.com>
2025-04-07 16:27:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-    origin: "http://localhost:5173", // Set this to your frontend URL
+    origin: "https://mybags.onrender.com",//http://localhost:5173 // Set this to your frontend URL
     credentials: true, // Allow cookies and authentication headers
 }));
 
