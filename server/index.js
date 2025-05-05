86f9cfbfd883a8dc033baaf8f550ef9eea75c186
HARSHIT <hdadheech46@gmail.com>
2025-05-05 17:47:28 +0530

    Update index.js
    adding trust proxy

--- server/index.js
+++ server/index.js
@@ -55,7 +55,7 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
     }
 };
-
+app.set('trust proxy', 1);
 app.use(session(sessionConfig));
 
 app.use(passport.initialize());
